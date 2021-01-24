08fd57034ae0013954bcb74ecb86a854a433f096
Tac Tacelosky <tacman@gmail.com>
2021-01-24 09:11:11 -0500

    fix path for media

--- bin/release-tasks.sh
+++ bin/release-tasks.sh
@@ -1,7 +1,7 @@
-wget https://github.com/collectiveaccess/providence/archive/1.7.9.zip && unzip 1.7.9.zip -d public
+#wget https://github.com/collectiveaccess/providence/archive/1.7.9.zip && unzip 1.7.9.zip -d public
 #https://github.com/collectiveaccess/providence/archive/1.7.9.zip
 #git clone --depth 1 git@github.com:collectiveaccess/providence public/providence
-cp config/providence-setup.php public/providence-1.7.9/setup.php
-mkdir -p public/providence-1.7.9/media/collectiveaccess
-chmod +rw public/providence-1.7.9/media/collectiveaccess
+#cp config/providence-setup.php public/providence-1.7.9/setup.php
+mkdir -p vendor/collectiveaccess/providence/media/collectiveaccess
+chmod +rw vendor/collectiveaccess/providence/media/collectiveaccess
 
