48ca802d3abb94671e460edf51d08e1696be426a
Tac Tacelosky <tacman@gmail.com>
2021-02-03 19:51:40 -0500

    hacks for use statements

--- bin/release-tasks.sh
+++ bin/release-tasks.sh
@@ -3,15 +3,21 @@
 #git clone --depth 1 git@github.com:collectiveaccess/providence public/providence
 #cp config/providence-setup.php vendor/collectiveaccess/providence/setup.php
 #rm -Rf vendor/collectiveaccess/providence/app/tmp
+#exit 1;
+# from https://github.com/FriendsOfPHP/PHP-CS-Fixer
+#mkdir --parents tools/php-cs-fixer
+#composer require --working-dir=tools/php-cs-fixer friendsofphp/php-cs-fixer
+
 mkdir -p vendor/collectiveaccess/providence/media/collectiveaccess
 chmod +rw vendor/collectiveaccess/providence/media/collectiveaccess
 cd vendor/collectiveaccess/providence/ && git checkout app && cd ../../..
-tools/php-cs-fixer/vendor/bin/php-cs-fixer fix vendor/collectiveaccess/providence/app/
-bin/console app:add-namespace
+find vendor/collectiveaccess/providence/app -type f -exec php -r 'file_put_contents($argv[1], preg_replace("/\n *((abstract |final )?class )/", "\n$1", file_get_contents($argv[1])));' {} \;
+#exit 1;
+#find vendor/collectiveaccess/providence/app/lib/Export/Base*.php -type f -exec sed  -e ':a;N;$!ba;s/\n *((final|abstract)? +class)/\n$1/g' {} \;
+tools/php-cs-fixer/vendor/bin/php-cs-fixer fix vendor/collectiveaccess/providence/app/ --rules=no_unused_imports
 bin/console c:c
-bin/console app:fix-classes
-vendor/bin/phpstan analyze  vendor/collectiveaccess/providence/app --memory-limit=1G --level=0 --autoload-file=config/setup.php
-
+bin/console app:fix
+#vendor/bin/phpstan analyze  vendor/collectiveaccess/providence/app --memory-limit=1G --level=0 --autoload-file=config/setup.php
 
 #sed -i 's/MediaUrl\Plugins/Plugins\MediaUrl/g' vendor/collectiveaccess/providence/app/lib/Plugins/MediaUrl/*.php 
 #sed -i 's/namespace CA/namespace CA\lib/g' vendor/collectiveaccess/providence/app/*.php 
--- src/Command/FixCommand.php
+++ src/Command/FixCommand.php
@@ -13,6 +13,8 @@ use Symfony\Component\Finder\Finder;
 
 class FixCommand extends Command
 {
+    private $uses = [];
+    private string $usesString;
     protected static $defaultName = 'app:fix';
     /**
      * @var LoggerInterface
@@ -29,7 +31,7 @@ class FixCommand extends Command
     {
         $this
             ->setDescription('Add a short description for your command')
-            ->addArgument('dir', InputArgument::OPTIONAL, 'Directory to process', "vendor/collectiveaccess/providence/app")
+            ->addArgument('dir', InputArgument::OPTIONAL, 'Directory to process', "vendor/collectiUveaccess/providence/app/")
             ->addOption('expand-classes', null, InputOption::VALUE_NONE, 'expand files that have multiple classes')
             ->addOption('add-namespaces', null, InputOption::VALUE_NONE, 'add namespace to top of file')
         ;
@@ -46,6 +48,16 @@ class FixCommand extends Command
 
 // check if there are any search results
         if ($finder->hasResults()) {
+            // crazy hack
+            foreach ($finder as $file) {
+                $absoluteFilePath = $file->getRealPath();
+                $this->uses[$this->getNamespace($file)] = $absoluteFilePath;
+            }
+            
+            // hack --
+            $classes = array_map(fn($class) => "use $class;", array_keys($this->uses) );
+            $this->usesString = join("\n", $classes);
+            
             foreach ($finder as $file) {
                 $absoluteFilePath = $file->getRealPath();
                 
@@ -58,40 +70,54 @@ class FixCommand extends Command
                 
                 if ($php = file_get_contents($absoluteFilePath))
                 {
+                    
                     $newPhp = $php;
                     // check to make sure that the filename matches the class name.  If not, create new files. 
-                    if (preg_match_all('/\n(class ([^ ]+)[^\n]*\n{(.*?)\n})/', $php, $mm, PREG_SET_ORDER)) {
+                    if (preg_match_all('/\n((final |abstract )?class ([^ ]+)[^\n]*\n{(.*?)\n})/sm', $php, $mm, PREG_SET_ORDER)) {
                         foreach ($mm as $idx => $m) 
                         {
-                            $className = $m[2];
+                            $className = $m[3];
                             if ($className <> $file->getFilenameWithoutExtension()) {
                                 // if exactly one, rename php.  Otherwise, create classes and rename to .txt or .orig
                                 if ($idx === 0) {
+
 //                                    $this->logger->alert(sprintf("Renaming %s to %s", $file->getFilename(), $className.'.php'));
                                     
                                 } else {
                                     $newFilename = $file->getPath() . sprintf('/%s.php', $className);
                                     $this->logger->alert(sprintf("creating $newFilename"));
-                                    
-                                    file_put_contents($newFilename, '<?php' . "\n\n" . $m[1]);
+
+                                    // We shouldn't need a user, since its in the same namespace
+                                    $newClassPhp = '<?php' . "\n\n" . $m[1];
+                                    file_put_contents($newFilename, $this->addNamespace($file, $newClassPhp));
+
                                     $newPhp = str_replace($m[1], "// see $newFilename for class $className\n", $newPhp);
 //                                    $this->addNamespace($file);
                                     // probably a related exception class
                                     $this->logger->alert(sprintf("Extra class in %s (%s)", $absoluteFilePath, $className));
                                     
-                                    if ($newPhp <> $php) {
-                                        file_put_contents($absoluteFilePath, $newPhp);
-                                    }
-                                    dd($m);
-                                    if (count($mm) > 2) {
-                                        dd($mm);
-                                    }
-                                    
-                                    dd($className, $file->getFilename());
+//                                    dd($m);
+//                                    if (count($mm) > 2) {
+//                                        dd($mm);
+//                                    }
+//                                    
+//                                    dd($className, $file->getFilename());
                                 }
-                            }
+                            } else {
+                                file_put_contents($absoluteFilePath, $this->addNamespace($file, $php));
+                                $output->writeln(
+                                    "Class and filename match " . $absoluteFilePath,
+                                    OutputInterface::VERBOSITY_VERBOSE
+                                );
 
+                            }
                         }
+                        
+                        
+                        
+                        
+                        
+                        
 //                        if (count($mm) == 1) {
 //                            // check that class name matches
 //                            $className = $mm[1];
@@ -109,6 +135,12 @@ class FixCommand extends Command
                         }
 
                     }
+
+                    // likely classes have been removed.
+                    if ($newPhp <> $php) {
+                        file_put_contents($absoluteFilePath, $this->addNamespace($file, $newPhp));
+                    }
+
                     if ($input->getOption('add-namespaces')) {
                         if (file_put_contents($absoluteFilePath, $php)) {
                             $io->success("File $absoluteFilePath written.");
@@ -130,20 +162,33 @@ class FixCommand extends Command
 
 
         $io->success('Finished ' . __CLASS__);
+        
+        dd($this->uses);
 
         return Command::SUCCESS;
     }
     
-    private function addNamespace(\SplFileInfo $file): ?string
+    private function getNamespace(\SplFileInfo $file): ?string
     {
-        $absoluteFilePath = $file->getRealPath();
-        
-        $php = file_get_contents($absoluteFilePath);
+        $namespace = null;
         if ( preg_match('|app|', $file->getPath()) ) {
             // hack! 
             $namespace = str_replace('vendor/collectiveaccess/providence/', '', $file->getPath());
-            $namespace = str_replace('app/lib','CA/lib', $namespace);
-            $namespace = str_replace('/','\\', $namespace);
+            $namespace = str_replace('app/lib', 'CA/lib', $namespace);
+            $namespace = str_replace('/', '\\', $namespace);
+        }
+        return $namespace;
+    }
+    
+    
+    private function addNamespace(\SplFileInfo $file, string $php): ?string
+    {
+        $absoluteFilePath = $file->getRealPath();
+        
+//        $php = file_get_contents($absoluteFilePath);
+        if ( preg_match('|app|', $file->getPath()) ) {
+            // hack! 
+            $namespace = $this->getNamespace($file);
             
             $autoloadMap = [
                 '__CA_LIB_DIR__' => 'CA\lib',
@@ -165,25 +210,31 @@ class FixCommand extends Command
             // we may also need to add use statements to any file that reference these classes.
             
             // really we just want the part between the namespace and the start of the class
-            if (preg_match('/<\?php.*?(class|function)/ms', $php, $mm)) {
+            if (preg_match('/(<\?php.*?\n)(abstract |final )?(class |function )/ms', $php, $mm)) {
 
-                $oldHeader = $mm[0];
+                $oldHeader = $mm[1];
 
 //                dd($header);
+                // we need to add a Use for every class that's in the 
                 $header = preg_replace_callback("/(include|require)_once\((.*?)\.[\"']\/(.*?).php[\"']\)/",
                     function($m) use ($autoloadMap) {
                         $prefix=$m[2];
                         if (array_key_exists($prefix, $autoloadMap)) {
-                            return sprintf("use %s\%s", $autoloadMap[$prefix], str_replace('/', '\\', $m[3])); 
+                            $use = sprintf("%s\%s", $autoloadMap[$prefix], str_replace('/', '\\', $m[3]));
+                            $this->uses[$use] = $m[0];
+                            return sprintf("// use %s; // %s", $use, $m[0]); 
                         } else {
                             return $m[0]; // dont replace
                         }
                     }, $oldHeader);
+                
+                $header  .=  $this->usesString;
 //                dd($header);
                 // get rid of the old namespace
                 $header = preg_replace('/namespace [^ ]+;/', '', $header);
                 // add the new one
                 $header = str_replace("<?php", "<?php\n\nnamespace $namespace;\n", $header);
+//                dd($header);
 
                 // replace the header
                 $php = str_replace($oldHeader, $header, $php);
@@ -192,6 +243,7 @@ class FixCommand extends Command
 //                array_push($keepAsIncludes, $file->getFilename());
 //                $this->logger->error("no class or function found", [$absoluteFilePath]);
             }
+            
 //            foreach ($autoloadMap as $constant=>$prefix) {
 //            }
 
